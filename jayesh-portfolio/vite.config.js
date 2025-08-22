19c1b33b16d5a8e5c8366d22cfe0807aedd5634c
jayeshvm <jayeshp.vmukti@gmail.com>
2025-08-22 15:07:12 +0530

    Deploy build

--- jayesh-portfolio/vite.config.js
+++ jayesh-portfolio/vite.config.js
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/My_Portfolio/', // <-- IMPORTANT: Replace with your repo name
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 })
+
